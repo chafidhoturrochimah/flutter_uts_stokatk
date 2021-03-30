2bbf95b2e7dd5d271ba54a158470069a77c4c599
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-03-30 10:50:09 +0700

    Membuat Data Model 1

--- lib/Models/Barang.dart
+++ lib/Models/Barang.dart
@@ -30,7 +30,46 @@ class Barang {
   set outBrg( value) => this._outBrg = value;
 
   get stokAkhir => this._stokAkhir;
-  set stokAkhir( value) => this._stokAkhir = value;;
+  set stokAkhir( value) => this._stokAkhir = value;
 
+  // konstruktor versi 1 untuk mengeset nilai
+  Barang(
+    this._kodeBrg, 
+    this._namaBrg, 
+    this._satuan, 
+    this._stokAwal, 
+    this._inBrg, 
+    this._outBrg, 
+    this._stokAkhir
+  );
 
+  // konstruktor versi 2: konversi dari Map ke Item
+  //untuk mengambil data dari sql yang tersimpan berbentuk Map
+  //setelah itu akan disimpan kembali dalam bentuk variabel
+  Barang.fromMap(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._kodeBrg = map['kodeBrg'];
+    this._namaBrg = map['namaBrg'];
+    this._satuan = map['satuan'];
+    this._stokAwal = map['stokAwal'];
+    this._inBrg = map['inBrg'];
+    this._outBrg = map['outBrg'];
+    this._stokAkhir = map['stokAkhir'];
+  }
+
+  //membuat method Map untuk melakukan update dan insert.
+  // konversi dari Item ke Map
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['kodeBrg'] = kodeBrg;
+    map['namaBrg'] = namaBrg;
+    map['satuan'] = satuan;
+    map['stokAwal'] = stokAwal;
+    map['inBrg'] = inBrg;
+    map['outBrg'] = outBrg;
+    map['stokAkhir'] = stokAkhir;
+    
+    return map;
+  } 
 }

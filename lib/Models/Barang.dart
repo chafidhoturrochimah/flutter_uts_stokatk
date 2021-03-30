bb823c34522605700a27cc624f3ce7ae10186e67
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-03-30 10:49:47 +0700

    membuat data model 1

---
+++ lib/Models/Barang.dart
@@ -0,0 +1,36 @@
+class Barang {
+  int _id;
+  String _kodeBrg;
+  String _namaBrg;
+  String _satuan;
+  int _stokAwal;
+  int _inBrg;
+  int _outBrg;
+  int _stokAkhir;
+
+  int get id => this._id;
+  set id(int value) => this._id = value;
+
+  get kodeBrg => this._kodeBrg;
+  set kodeBrg( value) => this._kodeBrg = value;
+
+  get namaBrg => this._namaBrg;
+  set namaBrg( value) => this._namaBrg = value;
+
+  get satuan => this._satuan;
+  set satuan( value) => this._satuan = value;
+
+  get stokAwal => this._stokAwal;
+  set stokAwal( value) => this._stokAwal = value;
+
+  get inBrg => this._inBrg;
+  set inBrg( value) => this._inBrg = value;
+
+  get outBrg => this._outBrg;
+  set outBrg( value) => this._outBrg = value;
+
+  get stokAkhir => this._stokAkhir;
+  set stokAkhir( value) => this._stokAkhir = value;;
+
+
+}
